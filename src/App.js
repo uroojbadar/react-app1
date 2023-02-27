71d3b78172ca670caf49f744881cf0cb0d075dcb
uroojbadar <uroojbadar9@gmail.com>
2023-02-27 20:50:38 +0500

    add

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Application
+          Learn React Application1
         </a>
       </header>
     </div>
